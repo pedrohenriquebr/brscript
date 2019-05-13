375c52ddaf473e678f8fd2d6d7ecd7826c08e3d1
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2019-05-13 13:46:05 -0300

    Updating parser

--- _parser.py
+++ _parser.py
@@ -6,6 +6,14 @@ class Parser(object):
         self.lexer = lexer
         self.current_token  = self.lexer.get_next_token()
     
+    def eat(self,token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error(token_type,self.lexer.column,self.lexer.line)
+    
+    def error(self,expected, column, line):
+        return Exception('Expected: {} ')
     def parse(self):
         print("Parsing..")
         return AST()
