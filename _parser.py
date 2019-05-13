c535889a97f759ca97c17e31aad3389c67f19980
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2019-05-13 14:39:45 -0300

    Add rules

--- _parser.py
+++ _parser.py
@@ -1,6 +1,6 @@
 from _lexer import *
 from _ast import *
-
+from tokens import *
 class Parser(object):
     def __init__(self,lexer):
         self.lexer = lexer
@@ -13,7 +13,24 @@ class Parser(object):
             self.error(token_type,self.lexer.column,self.lexer.line)
     
     def error(self,expected, column, line):
-        return Exception('Expected: {} ')
-    def parse(self):
-        print("Parsing..")
+        raise Exception('Syntax error at col: {}, line: {} !\nExpected: {}'.format(column,line,expected))
+    
+    '''
+    BLOCK: BLOCK
+    | LPAREN EXPR RPAREN
+    | LPAREN FUNC ARGS RPAREN
+    '''
+    def block(self):
+        self.eat(LPARAN)
+
+        if self.current_token.type == LPARAN:
+            self.block()
+        # elif self.current_token == ID:
+          # self._id()
+          
+        self.eat(RPARAN)
         return AST()
+    
+    def parse(self):
+        node  = self.block()
+        return node
